3d7dedebad057b9dd1205dfab9096fcd85ad42df
Rafaa Omar <rafaa.ommar@gmail.com>
2026-06-05 04:23:02 +0300

    navgate feaure

---
+++ MyPortfolio.Client/wwwroot/js/nav.js
@@ -0,0 +1,37 @@
+window.initNavObserver = () => {
+
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-group a");
+
+    if (!sections.length) return;
+
+    const observer = new IntersectionObserver((entries) => { //for each section, check if it's intersecting and which one is most visible
+        let mostVisible = null;
+        let maxRatio = 0;
+
+        entries.forEach(entry => {
+            if (entry.isIntersecting && entry.intersectionRatio > maxRatio) {
+                maxRatio = entry.intersectionRatio;
+                mostVisible = entry;
+            }
+        });
+
+        if (mostVisible) {
+
+            navLinks.forEach(link => link.classList.remove("active"));
+
+            const id = mostVisible.target.id;
+
+            const activeLink = document.querySelector(`.nav-group a[href="#${id}"]`);
+
+            if (activeLink) {
+                activeLink.classList.add("active");
+            }
+        }
+
+    }, {
+        threshold: [0.2, 0.4, 0.6, 0.8]
+    });
+
+    sections.forEach(sec => observer.observe(sec));
+};
